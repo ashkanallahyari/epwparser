250a9323f7d8522c3d0323d1241ead5813be59e2
Ashkan Allahyari <ashkan.allahyari@gmail.com>
2025-07-29 09:06:44 +0200

    updating codes

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import os
 from dotenv import load_dotenv
 
 data_update = EPWFilePreparator()
-print(data_update.raw_data_file_names[12])
+print(data_update.raw_data_file_names)
 for i, file in enumerate(data_update.list_files_needed_update()):
     data_update.parse_file(file)
     print (i)
